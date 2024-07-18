59a3bc358c42743a5cbce01f0d51d8071d3465ca
unknown <kariukibenard189@gmail.com>
2024-07-18 19:39:14 +0300

    Changed the click listener for the menu button.

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -1,8 +1,12 @@
 const humButton = document.querySelector(".hum-button");
 const sideDrawer = document.getElementById("side-drawer");
 
+if (!sideDrawer.style.display) {
+  sideDrawer.style.display = "none";
+}
+
 humButton.addEventListener("click", () => {
-  if ((sideDrawer.style.display === "none")) {
+  if (sideDrawer.style.display === "none" || sideDrawer.style.display === "") {
     sideDrawer.style.display = "block";
   } else {
     sideDrawer.style.display = "none";

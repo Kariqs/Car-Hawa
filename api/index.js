fa7abddd2dc442ce8c6d95ae03b896c668a02302
unknown <kariukibenard189@gmail.com>
2024-07-24 20:54:32 +0300

    Added some stylings to the product card.

--- api/index.js
+++ api/index.js
@@ -51,6 +51,10 @@ app.use(basicRoutes);
 app.use(authRoutes);
 app.use("/admin", adminRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).render("error/404.ejs");
+});
+
 mongoose
   .connect(MONGODBURI)
   .then((result) => {
--- models/product.js
+++ models/product.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const productSchema = new Schema({
+  category: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,

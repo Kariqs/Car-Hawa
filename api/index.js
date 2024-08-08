a48beacd4a05e8d7fc94c9bbf63b0db649ebfd68
unknown <kariukibenard189@gmail.com>
2024-08-08 11:32:05 +0300

    Worked on order placement.

--- api/index.js
+++ api/index.js
@@ -63,9 +63,9 @@ app.use(basicRoutes);
 app.use(authRoutes);
 app.use("/admin", adminRoutes);
 
-// app.use((req, res, next) => {
-//   res.status(404).render("error/404.ejs");
-// });
+app.use((req, res, next) => {
+  res.status(404).render("error/404.ejs");
+});
 
 mongoose
   .connect(MONGODBURI)
